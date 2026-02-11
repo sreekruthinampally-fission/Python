e649a90e9f8269254570ab0fe348ca80a4bd6a96
sreekruthinampally-fission <sreekruthi.nampally@fissionlabs.com>
2026-02-11 13:18:29 +0530

    Add files via upload

---
+++ basics/strings.py
@@ -0,0 +1,24 @@
+print("These are Strings")
+print("This is first line \nThis is second line")
+print("To print a \' within quotes we use a slash.")
+print("To concatenate strings" + " we use a +")
+phrase = "Operation Mount"
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.lower().islower())
+print(len(phrase))
+print(phrase[7])
+print(phrase.index("M"))
+print(phrase.index("o"))
+print(phrase.index("Oper"))
+print(phrase.replace("Mount","Everest"))
+print(phrase[-2])
+print(phrase.isalpha())
+new_phrase = "aeop"
+phrases = "is alphabetical in order"
+new_num = "234"
+print(new_phrase.isalpha())
+print(new_num.isdigit())
+print(phrases.title())
+print(phrase.capitalize())
