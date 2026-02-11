bf654f73951834a4692ef1315e130479920d0f09
sreekruthinampally-fission <sreekruthi.nampally@fissionlabs.com>
2026-02-11 13:46:02 +0530

    Add files via upload

---
+++ MODULES/modu.py
@@ -0,0 +1,13 @@
+import MODULES.calc as calc
+print(calc.add(10,2))
+
+from math import sqrt, factorial
+print(sqrt(16))
+print(factorial(6))
+
+import math as m
+print(m.pi)
+
+import random
+print(random.randint(1,5))
+
